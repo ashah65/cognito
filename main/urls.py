2d4fe2b830ea197f5e10a28acf1beec068e9a0f0
ashah65 <aksh.shah56@gmail.com>
2024-05-19 13:10:54 -0400

    job list

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-  path("request-job", views.request_a_job, name='request_job'),
+  path("requestAJob", views.request_a_job, name='request_job'),
 ]
 
